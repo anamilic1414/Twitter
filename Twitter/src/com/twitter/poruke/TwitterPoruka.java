1a6c5ac6f4faba8136ef3a197720f6c0e5a070a8
anamilic1414 <ana.milic.pz@gmail.com>
2016-03-26 19:50:44 +0100

    Napravljena dokumentacija za klasu TwitterPoruka, sve njene atribute i metode.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,14 +1,39 @@
 package com.twitter.poruke;
+/**
+ * Klasa koja predstavlja twitter poruku. Svaka poruka ima korisnika i tekst.
+ * @author korisnik
+ *
+ */
 
 public class TwitterPoruka {
 
+	/**
+	 * Naziv korisnika kao String
+	 */
 	private String korisnik;
+	
+	/**
+	 * Tekst poruke kao String
+	 */
 	private String poruka;
 	
+	/**
+	 * Metoda vraca vrednost atributa korisnik
+	 * @return
+	 */
 	public String getKorisnik() {
 	return korisnik;
 	}
 	
+	/**
+	 * Metoda postavlja novu vrednost za atribut korisnik.
+	 * @param korisnik Novi naziv korisnika
+	 * @throws java.lang.RuntimeException ako je unet naziv:
+	 * <ul>
+	 * 	<li>null</li>
+	 * 	<li>prazan String</li>
+	 * </ul>
+	 */
 	public void setKorisnik(String korisnik) {
 	if (korisnik==null || korisnik == "")
 	throw new RuntimeException(
@@ -16,10 +41,23 @@ public class TwitterPoruka {
 	this.korisnik = korisnik;
 	}
 	
+	/**
+	 * Metoda vraca vrednost atributa poruka.
+	 * @return
+	 */
 	public String getPoruka() {
 	return "poruka";
 	}
 	
+	/**
+	 * Metoda postavlja novu vrednost za atribut poruka.
+	 * @param poruka Nova poruka
+	 * @throws java.lang.RuntimeException ako je uneta poruka:
+	 * <ul>
+	 * 	<li>null</li>
+	 * 	<li>prazan String</li>
+	 * </ul>
+	 */
 	public void setPoruka(String poruka) {
 	if (poruka==null || this.poruka.length()>140)
 	throw new RuntimeException(
